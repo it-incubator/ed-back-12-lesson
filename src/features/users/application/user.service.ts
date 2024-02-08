7279c9f94573c0e24f9608c65edf884cf246f8c3
Tolkachev <vantrey@yandex.ru>
2024-02-08 10:40:02 +0300

    lesson-2 mongoose schema

--- src/features/users/application/user.service.ts
+++ src/features/users/application/user.service.ts
@@ -14,39 +14,8 @@ export class UserService {
   constructor(private userRepository: UsersRepository) {}
 
   async createUser(dto: CreateUserDto) {
-    const user = UserModel.createUser(dto);
+    const user = new UserModel(dto);
 
     await this.userRepository.save(user);
-  }
-
-  async convertMoney(id: string, from: string, to: string) {
-    const user = await this.userRepository.findById(id);
-
-    if (!user) {
-      throw new Error('not found');
-    }
-
-    user.convertMoney(from, to, 100);
-
-    await this.userRepository.save(user);
-  }
-
-  async transferMoney(dto: TransferMoneyDto) {
-    const fromUser = await this.userRepository.findByIdOrThrow(dto.fromUserId);
-    const toUser = await this.userRepository.findByIdOrThrow(dto.toUserId);
-
-    this.calculateTransfer(fromUser, toUser, dto);
-
-    await Promise.all([this.userRepository.save(fromUser), this.userRepository.save(toUser)]);
-  }
-
-  private calculateTransfer(
-    fromUser: UserDocument,
-    toUser: UserDocument,
-    dto: Omit<TransferMoneyDto, 'fromUserId' | 'toUserId'>
-  ) {
-    //logic
-    fromUser.decreaseWalletBalance(dto.fromUserWalletId, dto.amount);
-    toUser.increaseWalletBalance(dto.toUserWalletId, dto.amount);
   }
 }
--- src/features/users/domain/user.entity.ts
+++ src/features/users/domain/user.entity.ts
@@ -1,6 +1,5 @@
 import * as mongoose from 'mongoose';
 import { Model, model, HydratedDocument, ObjectId } from 'mongoose';
-import { CreateUserDto } from './dto';
 
 enum Currency {
   BYN = 'BYN',
@@ -21,12 +20,9 @@ type User = {
   wallets: Wallet[];
 };
 
-type UserMethods = typeof userMethods;
-type UserStatics = typeof userStatics;
+type UserModel = Model<User>;
 
-type UserModel = Model<User, {}, UserMethods> & UserStatics;
-
-export type UserDocument = HydratedDocument<User, UserMethods>;
+export type UserDocument = HydratedDocument<User>;
 
 const walletSchema = new mongoose.Schema<Wallet>({
   createdAt: { type: Date, required: true },
@@ -34,92 +30,14 @@ const walletSchema = new mongoose.Schema<Wallet>({
   currency: { type: String, enum: Currency, required: true },
 });
 
-type WalletModel = Model<Wallet, {}, {}> & typeof walletStatic;
-
-const walletStatic = {
-  createDefaultWallet() {
-    return new WalletModel({ createdAt: new Date(), balance: 100, currency: Currency.BTC });
-  },
-};
-
-walletSchema.statics = walletStatic;
-
-const WalletModel = model<Wallet, WalletModel>('wallet', walletSchema, 'users-l4');
-
-const userSchema = new mongoose.Schema<User, UserModel, UserMethods>(
+const userSchema = new mongoose.Schema<User>(
   {
     name: { type: String, required: true },
     age: { type: Number, required: true },
     wallets: { type: [walletSchema] },
   },
-  { optimisticConcurrency: true }
 );
 
-const userMethods = {
-  convertMoney(fromWalletId: string, toWalletId: string, amount: number) {
-    const fromWallet = (this as UserDocument).wallets.find(
-      (wallet) => wallet._id.toString() === fromWalletId
-    );
 
-    const toWallet = (this as UserDocument).wallets.find(
-      (wallet) => wallet._id.toString() === toWalletId
-    );
-
-    if (!fromWallet || !toWallet) {
-      throw new Error('some wallet not found');
-    }
-
-    fromWallet.balance = fromWallet.balance - amount;
-    toWallet.balance += amount;
-  },
-
-  increaseWalletBalance(walletId: string, amount: number) {
-    const wallet = (this as UserDocument).wallets.find(
-      (wallet) => wallet._id.toString() === walletId
-    );
-
-    if (!wallet) {
-      throw new Error('wallet not found');
-    }
-
-    wallet.balance += amount;
-  },
-
-  decreaseWalletBalance(walletId: string, amount: number) {
-    const wallet = (this as UserDocument).wallets.find(
-      (wallet) => wallet._id.toString() === walletId
-    );
-
-    if (!wallet) {
-      throw new Error('wallet not found');
-    }
-
-    wallet.balance -= amount;
-  },
-};
-
-const userStatics = {
-  createUser(dto: CreateUserDto) {
-    const user = new UserModel() as UserDocument;
-    user.age = dto.age;
-    user.name = dto.name;
-
-    if (user.age < 16) {
-      throw new Error('too yang');
-    }
-
-    if (dto.age < 18) {
-      user.wallets = [];
-
-      return user;
-    }
-
-    user.wallets = [WalletModel.createDefaultWallet()];
-    return user;
-  },
-};
-
-userSchema.methods = userMethods;
-userSchema.statics = userStatics;
 
 export const UserModel = model<User, UserModel>('users-l4', userSchema);
